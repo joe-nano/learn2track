e881ef6e3bafc00ffd63b7994e5ebd6884857567
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-10-13 00:26:16 -0400

    Cleaned a bit HCP processing script

--- scripts/las2ras_for_hcp.sh
+++ scripts/las2ras_for_hcp.sh
@@ -8,7 +8,7 @@ fi
 mv $1/T1w/Diffusion/bvals .
 mv $1/T1w/Diffusion/bvecs .
 mv $1/T1w/Diffusion/data.nii.gz .
-~/research/src/mrtrix3/bin/mrconvert data.nii.gz data_ras.nii.gz -stride 1,2,3,4 -fslgrad bvecs bvals -export_grad_fsl bvecs_ras bvals_ras -force
+mrconvert data.nii.gz data_ras.nii.gz -stride 1,2,3,4 -fslgrad bvecs bvals -export_grad_fsl bvecs_ras bvals_ras -force
 python ~/research/src/learn2track/scripts/extract_single_shell.py data_ras.nii.gz 1000 --bvals bvals_ras --bvecs bvecs_ras --basename dwi_ras
 python ~/research/src/scilpy/scripts/apply_crop_bb.py dwi_ras_b0.nii.gz dwi_ras_brain_b0.nii.gz box.pkl -f
 python ~/research/src/scilpy/scripts/apply_crop_bb.py dwi_ras_b1000.nii.gz dwi_ras_brain_b1000.nii.gz box.pkl -f
@@ -16,6 +16,5 @@ python ~/research/src/scilpy/scripts/apply_crop_bb.py dwi_ras_b1000.nii.gz dwi_r
 # Cleaning up
 mv -f dwi_ras_brain_b1000.nii.gz dwi_ras_b1000.nii.gz
 mv -f dwi_ras_brain_b0.nii.gz dwi_ras_b0.nii.gz
-rm -f data.nii.gz bvals bvecs
-rm -f data_ras_b0.nii.gz bvals_ras bvecs_ras data_ras.nii.gz
+rm data.nii.gz bvals bvecs data_ras_b0.nii.gz bvals_ras bvecs_ras data_ras.nii.gz
 rm -rf ./$1
