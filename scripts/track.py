5e2ea9a3a56b4d3a5b5d07016a5d10cb20c527b3
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-12-06 03:07:46 -0500

    BF: step_size was not in mm

--- scripts/track.py
+++ scripts/track.py
@@ -734,7 +734,7 @@ def main():
             filename = "{}_seeding-{}_step-{:.2f}mm_nbSeeds-{}_maxAngle-{:.1f}deg_keepCurv-{}_filtered-{}_minLen-{}_pftRetry-{}_pftHist-{}_useMaxComponent-{}.tck".format(
                 os.path.basename(args.name.rstrip('/'))[:6],
                 "wm" if "wm" in args.seeds[0] else "rois",
-                step_size,
+                args.step_size,
                 args.nb_seeds_per_voxel,
                 np.rad2deg(theta),
                 not args.discard_stopped_by_curvature,
