218ca68a10ad6bc5e4867ce6775539f6631f9d23
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2017-02-17 10:34:05 -0500

    Copy/paste TractographyData

--- scripts/viz.py
+++ scripts/viz.py
@@ -8,12 +8,143 @@ import sys
 sys.path = [os.path.abspath(os.path.join(__file__, '..', '..'))] + sys.path
 
 import numpy as np
+from collections import OrderedDict
+
 import nibabel as nib
+
 from dipy.viz import actor, window, widget
 from dipy.viz import fvtk
 from dipy.tracking.streamline import set_number_of_points
 
-from learn2track.neurotools import TractographyData
+
+class TractographyData(object):
+    def __init__(self, signal, gradients, name2id=None):
+        """
+        Parameters
+        ----------
+        signal : :class:`nibabel.Nifti1Image` object
+            Diffusion signal used to generate the streamlines.
+        gradients : :class:`dipy.core.gradients.GradientTable` object
+            Diffusion gradient information for the `signal`.
+        """
+        self.streamlines = nib.streamlines.ArraySequence()
+        self.bundle_ids = np.zeros((0,), dtype=np.int16)
+        self.name2id = OrderedDict() if name2id is None else name2id
+        self.signal = signal
+        self.gradients = gradients
+        self.subject_id = None
+        self.filename = None
+
+    @property
+    def volume(self):
+        if self._volume is None:
+            # Returns original signal
+            return self.signal.get_data()
+
+        return self._volume
+
+    @volume.setter
+    def volume(self, value):
+        self._volume = value
+
+    @property
+    def bundle_names(self):
+        return list(self.name2id.keys())
+
+    def add(self, streamlines, bundle_name=None, bundle_ids=None):
+        """ Adds a bundle of streamlines to this container.
+
+        Parameters
+        ----------
+        streamlines : `ArraySequence` object or list of 3D arrays
+            Streamlines to be added.
+        bundle_name : str
+            Name of the bundle the streamlines belong to.
+        """
+        # Get bundle ID, create one if it's new bundle.
+        if bundle_name is not None:
+            if bundle_name not in self.name2id:
+                self.name2id[bundle_name] = len(self.name2id)
+
+            bundle_id = self.name2id[bundle_name]
+
+        if bundle_ids is not None:
+            bundle_id = bundle_ids
+
+        # Append streamlines
+        self.streamlines.extend(streamlines)
+        size = len(self.bundle_ids)
+        new_size = size + len(streamlines)
+        self.bundle_ids.resize((new_size,))
+        self.bundle_ids[size:new_size] = bundle_id
+
+    @classmethod
+    def load(cls, filename):
+        data = np.load(filename)
+        streamlines_data = cls(data['signal'].item(), data['gradients'].item())
+        streamlines_data.filename = filename
+        streamlines_data.streamlines._data = data['coords']
+        streamlines_data.streamlines._offsets = data['offsets']
+        streamlines_data.streamlines._lengths = data['lengths']
+        streamlines_data.bundle_ids = data['bundle_ids']
+        streamlines_data.name2id = OrderedDict([(str(k), int(v)) for k, v in data['name2id']])
+        return streamlines_data
+
+    def save(self, filename):
+        np.savez(filename,
+                 signal=self.signal,
+                 gradients=self.gradients,
+                 coords=self.streamlines._data.astype(np.float32),
+                 offsets=self.streamlines._offsets,
+                 lengths=self.streamlines._lengths.astype(np.int16),
+                 bundle_ids=self.bundle_ids,
+                 name2id=list(self.name2id.items()))
+
+    def __str__(self):
+        import textwrap
+        msg = textwrap.dedent("""
+                              ################################################
+                              Dataset "{dataset_name}"
+                              ################################################
+                              ------------------- Streamlines ----------------
+                              Nb. streamlines:    {nb_streamlines:}
+                              Nb. bundles:        {nb_bundles}
+                              Step sizes (in mm): {step_sizes}
+                              Fiber nb. pts:      {fiber_lengths}
+                              --------------------- Image --------------------
+                              Dimension:     {dimension}
+                              Voxel size:    {voxel_size}
+                              Nb. B0 images: {nb_b0s}
+                              Nb. gradients: {nb_gradients}
+                              dwi filename:  {dwi_filename}
+                              affine: {affine}
+                              -------------------- Bundles -------------------
+                              {bundles_infos}
+                              """)
+
+        name_max_length = max(map(len, self.name2id.keys()))
+        bundles_infos = "\n".join([(name.ljust(name_max_length) +
+                                    "{}".format((self.bundle_ids==bundle_id).sum()).rjust(12))
+                                   for name, bundle_id in self.name2id.items()])
+
+        t = nib.streamlines.Tractogram(self.streamlines.copy())
+        t.apply_affine(self.signal.affine)  # Bring streamlines to RAS+mm
+        step_sizes = np.sqrt(np.sum(np.diff(t.streamlines._data, axis=0)**2, axis=1))
+        step_sizes = np.concatenate([step_sizes[o:o+l-1] for o, l in zip(t.streamlines._offsets, t.streamlines._lengths)])
+
+        msg = msg.format(dataset_name=self.filename,
+                         nb_streamlines=len(self.streamlines),
+                         nb_bundles=len(self.name2id),
+                         step_sizes="[{:.3f}, {:.3f}] (avg. {:.3f})".format(step_sizes.min(), step_sizes.max(), step_sizes.mean()),
+                         fiber_lengths="[{}, {}] (avg. {:.1f})".format(self.streamlines._lengths.min(), self.streamlines._lengths.max(), self.streamlines._lengths.mean()),
+                         dimension=self.signal.shape,
+                         voxel_size=tuple(self.signal.header.get_zooms()),
+                         nb_b0s=self.gradients.b0s_mask.sum(),
+                         nb_gradients=np.logical_not(self.gradients.b0s_mask).sum(),
+                         dwi_filename=self.signal.get_filename(),
+                         affine="\n        ".join(str(self.signal.affine).split('\n')),
+                         bundles_infos=bundles_infos)
+        return msg[1:]  # Without the first newline.
 
 
 def horizon(tractograms, data, affine):
